2ae2f700e9706859598334ccc4201432ee56354b
mune-kyun <rzknurfaizi@gmail.com>
2022-11-29 21:26:33 +0700

    feat: some inits

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from .models import HotelUser
 from hotel.models import Hotel
 from room.models import Room
+from roomservice.models import RoomService, RoomServiceOrder
 
 # Register your models here.
 
 admin.site.register(HotelUser)
 admin.site.register(Hotel)
 admin.site.register(Room)
+admin.site.register(RoomService)
+admin.site.register(RoomService, RoomServiceOrder)
