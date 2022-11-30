1158e1428b49c5a74b76b07fbd1ee209bf9e7463
fathanm10 <fathan.muhammad01@ui.ac.id>
2022-11-30 22:13:44 +0700

    feat: fully functional

--- room/forms.py
+++ room/forms.py
@@ -1,9 +1,13 @@
 from django import forms
+from django.forms import ModelForm
 from .models import Room
-from hotel.models import Hotel
 
-class RoomForm(forms.Form):
-    name = forms.CharField(label="name", max_length=255)
-    kapasitas_max = forms.IntegerField(label="kapasitas_max")
-    tipe = forms.CharField(label="tipe", max_length=255)
-    hotel = forms.ModelChoiceField(label="hotel", queryset=Hotel.objects.all().order_by('name'))
+class CreateRoomForm(ModelForm):
+    class Meta:
+        model = Room
+        exclude = ('status',)
+
+class EditRoomForm(ModelForm):
+    class Meta:
+        model = Room
+        fields = '__all__'
