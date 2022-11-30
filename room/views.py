7d67b2f204bd1c44f6dc3e04d0133a24f315f5a3
fathanm10 <fathan.muhammad01@ui.ac.id>
2022-11-30 16:18:13 +0700

    add rooms table view

--- room/views.py
+++ room/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from tkrpl.auth import *
+from .models import *
 
 # Create your views here.
+def manage_rooms(request):
+    if request.method != "POST":
+        if get_role(request) != "HOTEL_SUPERVISOR":
+            return redirect(reverse_lazy("home"))
+        rooms = {
+            "rooms": Room.objects.values()
+        }
+        return render(request, 'room/management.html', rooms)
+    
+    # return render(request, "room/create-edit.html")
--- tkrpl/urls.py
+++ tkrpl/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('room/', include('room.urls')),
     path('', include('main.urls')), # new
 ]
