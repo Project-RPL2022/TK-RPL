50fc1d7b3310bc39706707031f57bd01a0a519c5
mune-kyun <rzknurfaizi@gmail.com>
2022-12-02 18:36:09 +0700

    fix: logout path error

--- tkrpl/urls.py
+++ tkrpl/urls.py
@@ -21,7 +21,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    # path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('room/', include('room.urls')),
     path('roomservice/', include('roomservice.urls')),
     path('feedback/', include('feedback.urls')),
