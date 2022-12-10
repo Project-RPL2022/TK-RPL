74b5a4f1a5f044c5a0338e4a5bee655718d184ee
Ahmad Ghozi Fidinillah <ahmadghozi1234@gmail.com>
2022-12-10 20:18:22 +0700

    payment fe

--- payment/urls.py
+++ payment/urls.py
@@ -3,8 +3,13 @@ from .views import *
 
 
 urlpatterns = [
+    path("", payment_list),
     path("room_order/<int:room_payment_id>", pay_room_payment),
+    path("room_order/detail/<int:room_payment_id>", get_room_payment_detail),
+    path("api/room_order/detail/<int:room_payment_id>", get_room_payment_detail_api),
     path("room_service_order/<int:room_service_payment_id>", pay_room_service_payment),
+    path("room_service_order/detail/<int:room_service_payment_id>", get_room_service_payment_detail),
+    path("api/room_service_order/detail/<int:room_service_payment_id>", get_room_service_payment_detail_api),
     path("verify/", get_payment_info, name="payment-verification"),
     path("verify/<int:room_payment_id>", room_payment_verify),
     path("verify/<int:room_service_payment_id>", room_service_payment_verify),
